dc8cc74b6773b441da39d913b5ccdd5569de4c3b
Harshith KV <harshith.shetty2002@gmail.com>
2024-07-10 10:15:20 +0530

    ad

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,40 @@
-import express from "express";
-import dotenv from "dotenv";
-import databaseConnection from "./utils/database.js";
-import cookieParser from "cookie-parser";
-import userRoute from "./routes/userRoute.js";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import databaseconnection from './utils/database.js';
+import cookieParser from 'cookie-parser';
+import userRoute from './routes/userRoute.js'
+import cors from 'cors';
 
-// Load environment variables from .env file
-dotenv.config({ path: ".env" });
 
-// Connect to database
-databaseConnection();
+dotenv.config({
+    path: ".env"
+});
 
 const app = express();
 
-// Middlewares 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
     credentials: true
-};
+}
 app.use(cors(corsOptions));
 
-// API routes
-app.use("/api/v1/user", userRoute);
+// Database connection
+databaseconnection();
 
-// Start server
+
+// API 
+app.use("/api/v1/user", userRoute)
+
+
+
+/* http://localhost:8080/api/v1/user/register     */
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening at port ${process.env.PORT}`);
-})
+});
