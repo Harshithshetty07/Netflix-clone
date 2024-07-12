ed5f586272ae95c0ae5f42d5503f4a1f2750b7be
Harshith KV <harshith.shetty2002@gmail.com>
2024-07-12 09:51:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:3002",
+    origin: "https://netflix-clone-backend-ten.vercel.app/",
     credentials: true
 }
 app.use(cors(corsOptions));
